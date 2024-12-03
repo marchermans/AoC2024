92e86ba1d26bc12fe3e61c7457e3a02590dd690e
Marc Hermans <marc.hermans@ldtteam.com>
2024-12-03 15:13:35 +0100

    Day 3 - Part 1

---
+++ Day3.MatchMul/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+const string MUL_REGEX_PATTERN = "mul\\((?<left>[0-9]{1,3}),(?<right>[0-9]{1,3})\\)";
+var mulMatcher = new Regex(MUL_REGEX_PATTERN);
+
+var result = File
+    .ReadAllLines("input.txt")
+    .SelectMany(line => mulMatcher.Matches(line))
+    .Where(m => m.Success)
+    .GroupBy(m => m.Groups["left"].Value)
+    .SelectMany(m => m.Select(v => int.Parse(v.Groups["right"].Value) * int.Parse(m.Key)))
+    .Sum();
+
+Console.WriteLine("Result: " + result);
+    
