16f5d635abb9857973d6f6c09d0c508c2a17ed41
Marc Hermans <marc.hermans@ldtteam.com>
2024-12-03 16:24:53 +0100

    Day 2 - Complete linq based solution

--- Day3.EnabledMatchMul/Program.cs
+++ Day3.EnabledMatchMul/Program.cs
@@ -1,45 +1,21 @@
-﻿
+﻿// See https://aka.ms/new-console-template for more information
+
 using System.Text.RegularExpressions;
 
-const string MUL_REGEX_PATTERN = "^mul\\((?<left>[0-9]{1,3}),(?<right>[0-9]{1,3})\\)";
+const string MUL_REGEX_PATTERN = "mul\\((?<left>[0-9]{1,3}),(?<right>[0-9]{1,3})\\)";
 var mulMatcher = new Regex(MUL_REGEX_PATTERN);
 
-const string DO = "do()";
-const string DONT = "don't()";
-const string MULMAX = "mul(123,123)";
+const string DISABLED_REPLACE_PATTERN = "don\\'t\\(\\).*?do\\(\\)";
+var replacementMatcher = new Regex(DISABLED_REPLACE_PATTERN);
 
-var enabled = true;
-var current = 0;
+var result = File
+    .ReadAllLines("input.txt")
+    .Aggregate("", (current, line) => current + line, result => new[] {result})
+    .Select(line => replacementMatcher.Replace(line, ""))
+    .SelectMany(line => mulMatcher.Matches(line))
+    .Where(m => m.Success)
+    .GroupBy(m => m.Groups["left"].Value)
+    .SelectMany(m => m.Select(v => int.Parse(v.Groups["right"].Value) * int.Parse(m.Key)))
+    .Sum();
 
-//181339741
-//182780583
-foreach (var line in File.ReadAllLines("input.txt"))
-{
-    for (var i = 0; i < line.Length; i++)
-    {
-        var doBlock = line.Substring(i, Math.Min(line.Length - i, DO.Length));
-        var dontBlock = line.Substring(i, Math.Min(line.Length - i, DONT.Length));
-        var maxMulBlock = line.Substring(i, Math.Min(line.Length - i, MULMAX.Length));
-
-        switch (enabled)
-        {
-            case true when mulMatcher.IsMatch(maxMulBlock):
-            {
-                var match = mulMatcher.Match(maxMulBlock);
-                var left = int.Parse(match.Groups["left"].Value);
-                var right = int.Parse(match.Groups["right"].Value);
-            
-                current += left * right;
-                break;
-            }
-            case false when doBlock.Equals(DO):
-                enabled = true;
-                break;
-            case true when dontBlock.Equals(DONT):
-                enabled = false;
-                break;
-        } 
-    }
-}
-
-Console.WriteLine("Result: " + current);
+Console.WriteLine("Result: " + result);
